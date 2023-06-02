1c53cdcc5a754725c9db8930190ffaa164fb8ae3
mukulksingh98 <mukulksingh98@gmail.com>
2023-06-02 19:33:28 +1000

    home

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello World this is new");
+  res.send("Hello World this is new try it");
 });
 
 module.exports = router;
